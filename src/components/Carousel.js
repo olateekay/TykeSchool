36038a8f24f218194fc2cb84d79a4cf7147c5095
Olatokunbo <Olatokunbo@Javas-MacBook-Pro.local>
2019-07-29 12:07:41 +0100

    added images for the gallery

---
+++ src/components/Carousel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+    render () {
+        return (
+            <div className="carousel"></div>
+
+        );
+    }
+}
+
+export default Carousel;
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import styled from 'styled-components';
-import ttykeImagr from '../assests/ttykeImagr.jpg';
+import imager from '../assests/imager.jpg';
 
 const Styles = styled.div`
   .jumbotron {
-      background: url(${ttykeImagr}) no-repeat fixed bottom;
+      background: url(${imager}) no-repeat fixed bottom;
       background-size: cover;
       color: #efefef;
       height: 200px;
